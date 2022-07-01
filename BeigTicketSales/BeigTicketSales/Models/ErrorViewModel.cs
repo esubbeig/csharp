52b7c1df85ffeb71cfc1abdafad93a47f658b4f9
unknown <esubbeig786@gmail.com>
2022-07-01 01:11:30 -0500

    csharp projects

--- BeigTicketSales/BeigTicketSales/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BeigTicketSales.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- BeigWeek5/BeigWeek5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BeigWeek5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
